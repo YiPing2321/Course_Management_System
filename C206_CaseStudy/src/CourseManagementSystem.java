88054f367b7f3c5adaea483e65f047d427c4690b
19030407 <19030407@LAPTOP-2RBNFU3F>
2020-08-17 17:48:32 +0800

    Creation of Main class

---
+++ C206_CaseStudy/src/CourseManagementSystem.java
@@ -0,0 +1,12 @@
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 19030407, Aug 17, 2020 5:47:53 PM
+ */
+
+public class CourseManagementSystem {
+
+}
