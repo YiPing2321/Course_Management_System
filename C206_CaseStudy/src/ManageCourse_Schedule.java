13f5ef6c4cb236f966fe38259c235d20b4199efd
19030407 <19030407@LAPTOP-2RBNFU3F>
2020-08-22 07:32:49 +0800

    ManageCourse_Schedule class (children) created

---
+++ C206_CaseStudy/src/ManageCourse_Schedule.java
@@ -0,0 +1,20 @@
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 19030407, Aug 22, 2020 7:32:15 AM
+ */
+
+public class ManageCourse_Schedule {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
