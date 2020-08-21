ebd76371bac4c52817fe6949536b893924abb791
19030407 <19030407@LAPTOP-2RBNFU3F>
2020-08-22 07:35:57 +0800

    Creation of children class (ManageCategory, ManageCourse, ManageMember,ManageRegistration)

---
+++ C206_CaseStudy/src/ManageMember.java
@@ -0,0 +1,20 @@
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 19030407, Aug 22, 2020 7:34:05 AM
+ */
+
+public class ManageMember {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
