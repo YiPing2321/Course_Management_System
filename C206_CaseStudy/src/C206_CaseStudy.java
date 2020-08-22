0b7f174d5eca5109c15d89006c1634717b62c4c8
19030407 <19030407@LAPTOP-2RBNFU3F>
2020-08-22 11:22:01 +0800

    Refractor

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -1,33 +1,41 @@
 
 public class C206_CaseStudy {
 
+	private static final int Option_ManageMember = 1;
+	private static final int Option_ManageCategory = 2;
+	private static final int Option_ManageCourse = 3;
+	private static final int Option_ManageCourse_Schedule = 4;
+	private static final int Option_ManageRegistration = 5;
+	private static final int Option_Quit = 6;
+	
 	public static void main(String[] args) {
 		
 		
 		int option = 0;
 		
-		while (option != 6) {
+		while (option != Option_Quit) {
 			C206_CaseStudy.menu();
 			option = Helper.readInt("Enter an option > ");
 			
-			if (option == 1) {
+			if (option == Option_ManageMember) {
 				//Manage Members task - call method in class
 				ManageCategory.main(args);
-			} else if (option == 2) {
+			} else if (option == Option_ManageCategory) {
 				//Manage Category task - call method in class
 				ManageCategory.main(args);
-			} else if (option == 3) {
+			} else if (option == Option_ManageCourse) {
 				//Manage Course task - call method in class
 				ManageCourse.main(args);
-			} else if (option == 4) {
+			} else if (option == Option_ManageCourse_Schedule) {
 				//Manage Course Schedule task - call method in class
 				ManageCourse_Schedule.main(args);
-			} else if (option == 5) {
+			} else if (option == Option_ManageRegistration ) {
 				//Manage Registration task - call method in class
 				ManageRegistration.main(args);
-			} else if (option == 6) {
+			} else if (option == Option_Quit) {
 				//Quit
 				System.out.println("Thank you for using Course Management System!!");
+				
 			} else {
 				//Invalid option entered
 				System.out.println("Invalid Option");
