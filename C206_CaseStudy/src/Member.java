cd76ef2c3db8732c5c4dea08148ed8559d440257
19046377 <19046377@DESKTOP-E9JCCBO>
2020-08-23 14:52:27 +0800

    add new Member class

---
+++ C206_CaseStudy/src/Member.java
@@ -0,0 +1,12 @@
+
+/**
+ * I declare that this code was written by me.
+ * I will not copy or allow others to copy my code.
+ * I understand that copying code is considered as plagiarism.
+ *
+ * 19046377, Aug 23, 2020 2:52:08 PM
+ */
+
+public class Member {
+
+}
