673c93942d46ee578aafe0d50a977ee42de009fc
19009259 <19009259@DESKTOP-VF487TE>
2020-08-23 15:46:22 +0800

    -

--- C206_CaseStudy/test/ManageRegistrationTest.java
+++ C206_CaseStudy/test/ManageRegistrationTest.java
@@ -81,7 +81,7 @@ public class ManageRegistrationTest {
 		// Test if Item list is not null but empty - boundary
 		assertNotNull("Test if there is valid Course Category arraylist to delete item", registrationList);
 		
-		//Test that if the item have been deleted, the Course_Schedule List arraylist size is decrease by 1 
+		//Test that if the item have been deleted, the Registration List arraylist size is decrease by 1 
 		// which in this case is 1 - normal
 		int name = r1.getRegistration_no();
 		ManageRegistration.deleteRegistration(registrationList, r1.getRegistration_no());
