03b278b038f6f195d0acc582707787a4a68676fa
19030407 <19030407@LAPTOP-2RBNFU3F>
2020-08-24 18:54:52 +0800

    Update

--- C206_CaseStudy/test/ManageCourseTest.java
+++ C206_CaseStudy/test/ManageCourseTest.java
@@ -45,12 +45,7 @@ public class ManageCourseTest {
             // check if course code entered is not null (Error)
             assertTrue("Check that course code entered is not null", True);
         }
-<<<<<<< HEAD
-       
-        @Test
-=======
         
-<<<<<<< HEAD
         @Test
         public void DeleteCourseTest = (Test); {
             ManageCourse.viewCourse(courseList);
@@ -71,51 +66,7 @@ public class ManageCourseTest {
             ManageMember.deleteMembers(memberList);
             assertEquals("Check that memberList arraylist size is 0", 0, memberList.size());
         }
-=======
-        
-        @Test      
->>>>>>> branch 'master' of https://github.com/YiPing2321/Course_Management_System.git
-        public void DeleteCourseTest = (Test); {
-<<<<<<< HEAD
-            ManageCourse.viewCourse(courseList);
-            // check if course code entered is equals to the course list (Normal)
-            assertEquals("Check that course code entered is equals to the course list", courseList);
-                       
-            // check if course code entered is not equals to the course list (Error)
-            assertNotEquals("Check that course code entered is not equals to the course list", courseList);
-           
-            //Test that if the course have been deleted, the courseList arraylist size decrease by 1
-            //0 is normal
-            ManageCourse.deleteCourse(courseList);
-            assertEquals("Check that courserList arraylist size is 0", 0, courseList.size());
-        } 
-=======
-                ManageCourse.viewCourse(courseList);
-                //test course code entered is null
-                String courseCode = Helper.readString("Enter course code >");
-                for (int i = 0; i < courseList.size(); i++) {
-                    if (courseCode.equals(null)) {
-                        System.out.println("Please enter a course code to delete course");
-                    } else if (courseCode != null) {
-                        courseList.remove(i);
-                        System.out.println("Course deleted!");
-                    }
-                }
-                //test list not null but empty
-                assertNotNull("Test for valid arraylist to delete course from", courseList);
-                //Test that if the course have been deleted, the courseList arraylist size decrease by 1
-                //0 is normal
-                ManageCourse.deleteCourse(courseList);
-                assertEquals("Check that courserList arraylist size is 0", 0, courseList.size());
-            }
 
->>>>>>> branch 'master' of https://github.com/YiPing2321/Course_Management_System.git
-        
-        
-        
-
- 
->>>>>>> branch 'master' of https://github.com/YiPing2321/Course_Management_System.git
 
     }
 
